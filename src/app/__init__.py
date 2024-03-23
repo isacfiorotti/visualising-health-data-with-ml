2361702ab5238e5a5fadec0342493e797cab4276
isacfiorotti <115185340+isacfiorotti@users.noreply.github.com>
2024-03-23 20:31:07 +0000

    Move testing parameters and main() to dashboard_test.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,48 +1,14 @@
-import dash
-import dash_html_components as html
-import flask
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.serving import run_simple
+from flask import Flask
+from .dashboards.dashboard_test import init_dashboard
 
-import render as rd
+# Flask app
 
-#---------------------------------------------------------------------------------#
-
-# Testing parameters
-
-# Features to be displayed
-x_feat = "Time(s)"
-y_feat = "Arterial pressure (mmHg)"
-
-path = "/Users/isacfiorotti/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Modules/COMP4031 - Personal Project/data/data.csv"
-header_names = ['Time(s)', 'Arterial pressure (mmHg)', 'Aortic Doppler (kHz)',
-       'Mesenteric Doppler (kHz)', 'Renal Doppler (kHz)']
-
-width = 1000
-height = 400
-
-#---------------------------------------------------------------------------------#``
-
-def main():
-
-    # Read data
-    ddf = rd.read_csv(path, header_names)
-    rd.plot_raster(ddf, width, height, x_feat, y_feat)
-
-    run_simple('localhost', 8050, server)
-
-    return None
-
-#---------------------------------------------------------------------------------#
-
-# Create a Flask app that serves the Dash app which is created from main.py
-server = flask.Flask(__name__)
-
-@server.route('/')
-def index():
-    return 'Hello, World!'
-
-#---------------------------------------------------------------------------------#
+def init_app():
+    app = Flask(__name__)
     
-if __name__ == "__main__":
-    main()
+    with app.app_context():
+        from . import routes
+        app = init_dashboard(app)
+
+    return app
+
--- src/app/dashboards/dashboard_test.py
+++ src/app/dashboards/dashboard_test.py
@@ -1,9 +1,34 @@
 import dash
 from dash import dcc, html
 import plotly.graph_objs as go
+from app import render as rd
+import plotly.express as px
+
+#---------------------------------------------------------------------------------#
+
+# Testing parameters
+
+# Features to be displayed
+x_feat = "Time(s)"
+y_feat = "Arterial pressure (mmHg)"
+
+path = "/Users/isacfiorotti/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Modules/COMP4031 - Personal Project/data/data.csv"
+header_names = ['Time(s)', 'Arterial pressure (mmHg)', 'Aortic Doppler (kHz)',
+       'Mesenteric Doppler (kHz)', 'Renal Doppler (kHz)']
+
+width = 1000
+height = 400
+
+#---------------------------------------------------------------------------------#
+
+def build_graph():
+    ddf = rd.read_csv(path, header_names)
+    img = rd.rasterise(ddf.compute(), width, height, x_feat, y_feat)
+    fig = px.imshow(img)
+    return fig
+
 
 def init_dashboard(server):
-    """Create a Plotly Dash dashboard."""
     dash_app = dash.Dash(
         server=server,
         routes_pathname_prefix='/dashapp/'
@@ -13,19 +38,7 @@ def init_dashboard(server):
         html.H1('My Dash Application'),
         dcc.Graph(
             id='dash-container',
-            figure={
-                'data': [
-                    go.Scatter(
-                        x=[1, 2, 3, 4],
-                        y=[10, 15, 13, 17],
-                        mode='lines+markers',
-                        name='Line chart'
-                    )
-                ],
-                'layout': {
-                    'title': 'Dash Data Visualization'
-                }
-            }
+            figure=build_graph()
         )
     ])
 
