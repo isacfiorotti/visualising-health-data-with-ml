acc44ac0e5f82996fa43daa203cd783bf21c5c7b
isacfiorotti <115185340+isacfiorotti@users.noreply.github.com>
2024-03-23 09:13:49 +0000

    Add wrapper functions for reading and plotting

--- src/render.py
+++ src/render.py
@@ -2,76 +2,81 @@ import dask.dataframe as dd
 import datashader as ds
 import datashader.transfer_functions as tf
 import plotly.express as px
-
-#---------------------------------------------------------------------------------#
-# Testing parameters
-
-# Features to be displayed
-x_feat = "Time(s)"
-y_feat = "Arterial pressure (mmHg)"
-
-path = "/Users/isacfiorotti/Library/CloudStorage/OneDrive-TheUniversityofNottingham/Modules/COMP4031 - Personal Project/data/data.csv"
-header_names = ['Time(s)', 'Arterial pressure (mmHg)', 'Aortic Doppler (kHz)',
-       'Mesenteric Doppler (kHz)', 'Renal Doppler (kHz)']
-
-#plot dimensions
-width = 1000
-height = 400
+import datashader as ds
+import datashader.transfer_functions as tf
 
 #---------------------------------------------------------------------------------#
 
-#TODO: Make wrapper function to read data that is able to handle more features 
-
-ddf = dd.read_csv(path, 
-                 header=0, 
-                 names=header_names)
-
-ddf = ddf.persist() # Persist for future operations? I think that there might be a problem with this ask for help from big data people
-
-#---------------------------------------------------------------------------------#
-
+#TODO: Add functionality to infer header names from the CSV file
 #TODO: Add functionality to wrapper function to check that the program has been able to read all data types and converts time to interpretable time
 
-ddf['Time(s)'] = dd.to_datetime(ddf['Time(s)'], format='%H:%M:%S.%f').astype('int64')
+def read_csv(path, header_names):
+    """
+    Reads data from a CSV file using Dask.
+
+    Args:
+        path (str): The path to the CSV file.
+        header_names (list): A list of column names for the CSV file.
+
+    Returns:
+        ddf.datframe: A dask DataFrame containing the data.
+    """
+
+    ddf = dd.read_csv(path, 
+                     header=0, 
+                     names=header_names)
+
+    ddf = ddf.persist() # Persist for future operations? I think that there might be a problem with this ask for help from big data people
+
+    #Temporary function to convert time to interpretable time
+    ddf['Time(s)'] = dd.to_datetime(ddf['Time(s)'], format='%H:%M:%S.%f').astype('int64')
+
+    return ddf
 
 #---------------------------------------------------------------------------------#
 
-def rasterise(df, width, height):
-    """Rasterises Dask DataFrame using Datashader.
+def rasterise(df, width, height, x_feat, y_feat):
+    """
+    Rasterises Dask DataFrame using Datashader.
 
     Args:
-        df (ddf.dataframe.DataFrame): A dask DataFrame containing the data to be plotted.
+        df (ddf.dataframe): A dask DataFrame containing the data to be plotted.
         width (int): The width of the rendered image.
         height (int): The height of the rendered image.
+        x_feat (str): The name of the column in the DataFrame to be used as the x-axis.
+        y_feat (str): The name of the column in the DataFrame to be used as the y-axis.
 
     Returns:
         PIL.Image.Image: The rendered image of the line plot.
     """
-
-    canvas = ds.Canvas(
-        plot_width=width,
-        plot_height=height
-    )
-    agg = canvas.line(
-        df,
-        x=x_feat,
-        y=y_feat,
-    )
+    canvas = ds.Canvas(plot_width=width, plot_height=height)
+    agg = canvas.line(df, x=x_feat, y=y_feat)
     return tf.shade(agg, how='linear')
 
 #---------------------------------------------------------------------------------#
 
-#TODO: Make function that takes rasterise input and plots it
+#TODO: Add ability to choose different type of graph
 
-img = rasterise(ddf.compute()) # Always make sure to .compute() or you will not pass a ddf
+def plot_raster(df, width, height, x_feat, y_feat):
+    """
+    Plots the rasterised image using Plotly Express.
 
-# Plotly express plot
-fig = px.imshow(img)
-fig.update_layout(
-    title='Line Plot using Datashader and Plotly',
-)
-fig.show()
-print("Image displayed successfully.")
+    Args:
+        df (ddf.dataframe): A dask DataFrame containing the data to be plotted.
+        width (int): The width of the rendered image.
+        height (int): The height of the rendered image.
+        x_feat (str): The column name for the x-axis.
+        y_feat (str): The column name for the y-axis.
+    """
+    img = rasterise(df.compute(), width, height, x_feat, y_feat) # Always make sure to .compute() or you will not pass a ddf
+
+    # Plotly express plot
+    fig = px.imshow(img)
+    fig.update_layout(
+        title='Line Plot using Datashader and Plotly',
+    )
+    fig.show()
+    print("Image displayed successfully.")
 
 #---------------------------------------------------------------------------------#
 
