f9258394d184ce8c058c9c2930b0caee4ac94bac
isacfiorotti <115185340+isacfiorotti@users.noreply.github.com>
2024-03-23 17:22:16 +0000

    Rename main.py to __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,9 @@
+import dash
+import dash_html_components as html
+import flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+
 import render as rd
 
 #---------------------------------------------------------------------------------#
@@ -15,7 +21,7 @@ header_names = ['Time(s)', 'Arterial pressure (mmHg)', 'Aortic Doppler (kHz)',
 width = 1000
 height = 400
 
-#---------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#``
 
 def main():
 
@@ -23,7 +29,20 @@ def main():
     ddf = rd.read_csv(path, header_names)
     rd.plot_raster(ddf, width, height, x_feat, y_feat)
 
+    run_simple('localhost', 8050, server)
+
     return None
 
+#---------------------------------------------------------------------------------#
+
+# Create a Flask app that serves the Dash app which is created from main.py
+server = flask.Flask(__name__)
+
+@server.route('/')
+def index():
+    return 'Hello, World!'
+
+#---------------------------------------------------------------------------------#
+    
 if __name__ == "__main__":
     main()
