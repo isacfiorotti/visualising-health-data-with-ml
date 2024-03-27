fd6a8ddbd10e2d27cff71c80771e4dbc43422ce7
isacfiorotti <115185340+isacfiorotti@users.noreply.github.com>
2024-03-27 11:04:23 +0000

    Add display dask as desktop application

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,6 +1,9 @@
 from app import init_app
+from flaskwebgui import FlaskUI
 
 app = init_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+
+    # Deploys the python app as a desktop application using flaskwebgui
+    FlaskUI(app=app, server="flask").run()
